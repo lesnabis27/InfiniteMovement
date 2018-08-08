96f4e6d6e5ed3c627c4435f7adf6e33867644f59
Sam Richardson <lesnabis@icloud.com>
2018-08-08 15:55:44 -0400

    Initial Commit

---
+++ Infinite Movement/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Infinite Movement
+//
+//  Created by Sam Richardson on 8/8/18.
+//  Copyright © 2018 Sam Richardson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
